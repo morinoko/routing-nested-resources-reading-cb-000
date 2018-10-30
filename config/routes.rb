74a90f3c599e9b502b2feda19af030b368df7911
Felice Forby <feliceforby@gmail.com>
2018-10-30 20:45:23 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
-  get 'authors/:id/posts', to: 'authors#posts_index'
-  get 'authors/:id/posts/:post_id', to: 'authors#post'
+  resources :authors, only: [:show] do
+    # nested resource for posts
+    resources :posts, only: [:show, :index]
+  end
+  
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
