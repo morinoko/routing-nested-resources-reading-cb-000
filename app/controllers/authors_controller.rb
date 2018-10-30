c9215854dcbe833c2114dcc197fb34b8762f5cab
Felice Forby <feliceforby@gmail.com>
2018-10-30 20:36:42 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,13 +6,13 @@ class AuthorsController < ApplicationController
 
   def posts_index
     @author = Author.find(params[:id])
-    @post = @author.posts
+    @posts = @author.posts
 
     render template: 'posts/index'
   end
 
   def post
-    
+
   end
 
 end
