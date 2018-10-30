d574120880e480efb35d71a8d520317c097f81e5
Felice Forby <feliceforby@gmail.com>
2018-10-30 20:30:18 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   resources :authors, only: [:show]
+  get 'authors/:id/posts'
+  get 'authors/:id/posts/:post_id'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
