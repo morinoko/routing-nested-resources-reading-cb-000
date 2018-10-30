fd422f6243aca46d149128064b82e9fe2dca7c51
Felice Forby <feliceforby@gmail.com>
2018-10-30 20:38:16 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,11 +2,9 @@ class AuthorsController < ApplicationController
   before_action :set_author, only: [:show, :posts_index, :post]
 
   def show
-    @author = Author.find(params[:id])
   end
 
   def posts_index
-    @author = Author.find(params[:id])
     @posts = @author.posts
 
     render template: 'posts/index'
@@ -16,7 +14,7 @@ class AuthorsController < ApplicationController
 
   end
 
-  private 
+  private
 
   def set_author
     @author = Author.find(params[:id])
